cf6f6c225f99349998c4825aab7205b2ce6d54a3
Alexandre Rio <contact@alexrio.fr>
2014-07-27 13:33:57 +0200

    Add problem 027

--- Problem_027/src/main.cpp
+++ Problem_027/src/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <math.h>
+
+bool isPrime(double value)
+{
+  value = abs(value);
+  if (value == 2)
+    return true;
+  else {
+    double root = sqrt(value);
+    for (double i=2; i<=root; i++)
+      if (fmod(value,i) == 0)
+        return false;
+
+  }
+  return true;
+}
 
 int main()
 {
+  int n = 0;
+  int numberOfSuccesivePrime = 0;
+  int longestSuccesivePrime = 0;
+  int longest_a = 0;
+  int longest_b = 0;
+
+  for (int a=-999; a<1000; a++) {
+    for (int b=-999; b<1000; b++) {
+      numberOfSuccesivePrime = 0;
+      n = 0;
+
+      while (isPrime(n*n + a*n + b)) {
+        n++;
+        numberOfSuccesivePrime++;
+      }
+
+      if (numberOfSuccesivePrime > longestSuccesivePrime) {
+        longestSuccesivePrime = numberOfSuccesivePrime;
+        longest_a = a--;
+        longest_b = b--;
+      }
+    }
+  }
+
+  std::cout << "Longest succession: " << longestSuccesivePrime << std::endl;
+  std::cout << "a: " << longest_a << " and b: " << longest_b << " (product: " << longest_a*longest_b << " )" << std::endl;
   return 0;
 }
